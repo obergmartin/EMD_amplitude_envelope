75eb79968fbe7020370ff99f9af7aa0b4db834a4
Martin Oberg <obergma@gmail.com>
2024-07-05 12:07:57 -0700

    feat: loop through files in directory

--- envmetrics/envm_example.m
+++ envmetrics/envm_example.m
@@ -1,6 +1,13 @@
 %
+path = './';
+var_names = {'fn', 'sbpr','scntr','pow_imf1','pow_imf2','mu_w1','mu_w2','var_w1','var_w2','imf_ratio21'};
+results = cell2table(cell.empty(0,numel(var_names)), 'VariableNames', var_names);
+files = dir([path '*.wav']);
+do_plots = 0;
 
-[wav,Fs] = audioread('example.wav');
+for i = 1:length(files)
+fn = files(i).name;
+[wav,Fs] = audioread(fn);
 t_wav = linspace(0,length(wav)-1,length(wav))/Fs;
 
 par.bandpass = [400 4000];  %: vocalic energy bandpass range (there is no a priori justification for this range)
@@ -43,6 +50,7 @@ w = num2cell(w,1);
 
 
 %% plot waveform, envelope, smoothed envelope spectrum, imfs, and instantaneous frequencies
+if do_plots
 figure;
 subplot(4,3,1); plot(t_wav,wav); axis tight;
 subplot(4,3,4); plot(t_env,envw); axis tight;
@@ -53,14 +61,30 @@ for i=1:length(imf)
     subplot(4,3,3*i-1); plot(t_imf,imf{i}); hold on; axis tight; title(sprintf('imf %i',i));
     subplot(4,3,3*i); plot(t_imf,w{i}); hold on; axis tight; title(sprintf('imf %i instantaneous freq.',i));
 end
+end
 
 %% metrics
 par.powerratio_freq_bins = [1 3.5 10];
 par.centroid_freq_bins = [1 10];
 
-SPEC = envm_psd_metrics(smpsd,f,par) %#ok<*NOPTS>
-EMD = envm_emd_metrics(imf,w)
+SPEC = envm_psd_metrics(smpsd,f,par); %#ok<*NOPTS>
+EMD = envm_emd_metrics(imf,w);
+resultsrow = cell2table(cell(1,numel(var_names)), 'VariableNames', var_names);
 
+resultsrow.fn = {fn};
+resultsrow.sbpr = SPEC.sbpr_1;
+resultsrow.scntr = SPEC.scntr_1;
+resultsrow.pow_imf1 = EMD.pow_imf(1);
+resultsrow.pow_imf2 = EMD.pow_imf(2);
+resultsrow.mu_w1 = EMD.mu_w(1);
+resultsrow.mu_w2 = EMD.mu_w(2);
+resultsrow.var_w1 = EMD.var_w(1);
+resultsrow.var_w2 = EMD.var_w(2);
+resultsrow.imf_ratio21 = EMD.imf_ratio21;
+results = [results; resultsrow];
 
+end
+results;
+writetable(results, 'results.txt')
 
 
